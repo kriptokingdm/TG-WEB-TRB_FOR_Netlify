831d9fb34075078b54d8e1c66ea667c9432315ab
kriptokingdm <kriptokingdm@gmail.com>
2025-12-03 05:30:10 +0300

    Fix styles and add debug logs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+console.log('React app starting...');
+
 function TestApp() {
   return React.createElement('div', {
     style: {
@@ -8,10 +10,22 @@ function TestApp() {
       fontSize: '32px',
       backgroundColor: 'red',
       color: 'white',
-      textAlign: 'center'
+      textAlign: 'center',
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
     }
-  }, 'WORKING!');
+  }, '? SITE IS WORKING!');
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement(TestApp));
+const rootElement = document.getElementById('root');
+console.log('Root found:', !!rootElement);
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(React.createElement(TestApp));
+  console.log('React rendered successfully!');
+} else {
+  console.error('Root element not found!');
+}
